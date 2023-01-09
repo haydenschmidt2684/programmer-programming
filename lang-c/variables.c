f536e1e293f17acef41d846fd152bb0ffaa6214d
Hayden Schmidt <haydenschmidt2684@gmail.com>
2023-01-09 22:15:07 +0700

    Update stuff

---
+++ lang-c/variables.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+#define K_HELLO_WORLD "Something new here"
+
+int something = 0;
+__int8_t i8_number = 0;
+__int16_t i16_number = 0;
+__int32_t i32_number = 0;
+__int64_t i64_number = 0;
+
+bool isSomething = false;
+
+int main(){
+    printf("");
+    return 0;
+}
