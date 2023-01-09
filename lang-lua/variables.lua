85776c8414bff616454fe3f953cd20bcb30ee4e1
Hayden Schmidt <haydenschmidt2684@gmail.com>
2023-01-09 21:04:46 +0700

    Update lua

---
+++ lang-lua/variables.lua
@@ -0,0 +1,12 @@
+--number-variables
+local i_number = 0
+local d_number = 0.15
+
+--string
+local chr_something = 'a'
+local str_something = "Hello there !!"
+
+--arrays
+local sz_something = {}
+
+print(str_something)
